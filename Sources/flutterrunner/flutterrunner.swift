297e64570f61a9e8c5849e2a7ad5fde73531342e
Tuğcan ÖNBAŞ <tugcanonbas@hotmail.com>
2022-12-20 13:52:08 +0300

    device model

--- Sources/flutterrunner/flutterrunner.swift
+++ Sources/flutterrunner/flutterrunner.swift
@@ -10,66 +10,24 @@ public struct flutterrunner {
         let pipe = Pipe()
         devicesProcess.standardOutput = pipe
         devicesProcess.standardError = pipe
-        devicesProcess.arguments = ["flutter", "devices"]
+        devicesProcess.arguments = ["flutter", "devices", "--machine"]
         devicesProcess.launch()
         devicesProcess.waitUntilExit()
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        guard let devices = String(data: data, encoding: .utf8) else {
-            print("Error: flutter devices failed")
+        devicesProcess.terminate()
+
+        guard let devices = Device.fromJSON(data) else {
+            print("Could not parse devices")
             return
         }
-        devicesProcess.terminate()
-        print("Devices: \(devices)")
-
-// Output:
-// Pixel 5 (mobile)            • 12091FDD4001BF                       • android-arm64  • Android 13 (API 33)
-// Digitastic SE 2020 (mobile) • 00008030-0011493A3E45802E            • ios            • iOS 16.1.2 20B110
-// iPhone 14 Pro Max (mobile)  • F8668852-E0CD-4811-BDE0-91362BE11DA5 • ios            • com.apple.CoreSimulator.SimRuntime.iOS-16-2 (simulator)
-// macOS (desktop)             • macos                                • darwin-arm64   • macOS 13.1 22C65 darwin-arm
-// Chrome (web)                • chrome                               • web-javascript • Google Chrome 108.0.5359.124
-
-// Select device ids from output without macOS and Chrome
-
-
-        let lines = devices.components(separatedBy: "\n")
-
-        // print("Lines: \(lines)")
-
-        let selectedLines = lines.filter { line in
-            return line.contains("mobile")
-        }
-
-        // print("Selected lines: \(selectedLines)")
-
-        var selectedDevicesUUIDs = [String]()
-
-        for line in selectedLines {
-            let components = line.components(separatedBy: "•")
-            let uuid = components[1].trimmingCharacters(in: .whitespaces)
-            selectedDevicesUUIDs.append(uuid)
-        }
-
-        // print(selectedDevicesUUIDs)
 
         var processes = [Process]()
         var pipes = [Pipe]()
-        selectedDevicesUUIDs.forEach { uuid in
-            let process = Process()
-            process.launchPath = "/usr/bin/env"
-            let pipe = Pipe()
-            // process.standardOutput = pipe
-            // process.standardError = pipe
-            process.standardInput = pipe
 
-            let file = pipe.fileHandleForReading
-            file.readabilityHandler = { file in
-                if let _ = String(data: file.availableData, encoding: .utf8) {
-                    // print(file)
-                    print("Running on \(uuid)")
-                }
-            }
-            
-            process.arguments = ["flutter", "run", "-d", uuid]
+        devices.forEach { device in
+            print("Device: \(device.name), \(device.targetPlatform), (\(device.id))")
+            let (process, pipe) = device.run()
+
             processes.append(process)
             pipes.append(pipe)
         }
@@ -78,13 +36,19 @@ public struct flutterrunner {
         for process in processes {
             let tmpPipe = Pipe()
             process.standardOutput = tmpPipe
+
+            defer {
+                process.terminate()
+            }
+
             process.launch()
 
             while true {
                 let data = tmpPipe.fileHandleForReading.availableData
                 if let string = String(data: data, encoding: .utf8) {
-                    print(string)
+                    // print(string)
                     if string.contains("Running with sound null safety") {
+                        print("Running on \(process.arguments![3])")
                         break
                     }
                 }
@@ -110,4 +74,41 @@ filewatcher.start()
 
 RunLoop.main.run()
     }
+}
+
+struct Device: Decodable {
+    let name: String
+    let id: String
+    let isSupported: Bool
+    let targetPlatform: String
+    let emulator: Bool
+    let sdk: String
+    let capabilities: Capabilities
+
+    static func fromJSON(_ data: Data) -> [Device]? {
+        return try? JSONDecoder().decode([Device].self, from: data)
+    }
+
+    func run() -> (Process, Pipe) {
+        let process = Process()
+            process.launchPath = "/usr/bin/env"
+            let pipe = Pipe()
+            // process.standardOutput = pipe
+            // process.standardError = pipe
+            process.standardInput = pipe
+            
+            process.arguments = ["flutter", "run", "-d", self.id]
+
+            return (process, pipe)
+    }
+}
+
+struct Capabilities: Decodable {
+    let hotReload: Bool
+    let hotRestart: Bool
+    let screenshot: Bool
+    let fastStart: Bool
+    let flutterExit: Bool
+    let hardwareRendering: Bool
+    let startPaused: Bool
 }
